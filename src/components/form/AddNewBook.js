670c5814bcd700b65dbdb99335c29adbd59be850
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-01-18 16:40:09 +0100

    Dispatch action to submit book to redux store

--- src/components/form/AddNewBook.js
+++ src/components/form/AddNewBook.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-// import { v4 as uuidv4 } from 'uuid';
-// import { Provider, useDispatch } from 'react-redux';
-// import { addBook } from '../../redux/books/books';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../../redux/books/books';
 
 function AddNewBook() {
   const [title, setTitle] = useState('');
   const [author, setAuthur] = useState('');
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   const handleTitle = (e) => {
     setTitle(e.target.value);
@@ -16,49 +16,50 @@ function AddNewBook() {
     setAuthur(e.target.value);
   };
 
-  // const submitBookToStore = () => {
-  //   const newBook = {
-  //     id: uuidv4(), // make sure it's unique
-  //     title,
-  //     author,
-  //   };
+  const submitBookToStore = () => {
+    const newBook = {
+      id: uuidv4(), // make sure it's unique
+      title,
+      author,
+    };
 
-  //   // dispatch an action and pass it the newBook object (your action's payload)
-  //   dispatch(addBook(newBook));
-  // };
+    // dispatch an action and pass it the newBook object (your action's payload)
+    dispatch(addBook(newBook));
+  };
 
   return (
-      <form>
-        <input
-          type="text"
-          placeholder="Title"
-          value={title}
-          onChange={handleTitle}
-        />
-        <input
-          type="text"
-          placeholder="Authur"
-          value={author}
-          onChange={handleAuthur}
-        />
-        <label htmlFor="Category">
-          Category
-          <select name="Category" id="Category">
-            <option value="Action">Action</option>
-            <option value="Science Fiction">Science Fiction</option>
-            <option value="Economy">Economy</option>
-          </select>
-        </label>
+    <form>
+      <input
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={handleTitle}
+      />
+      <input
+        type="text"
+        placeholder="Authur"
+        value={author}
+        onChange={handleAuthur}
+      />
+      <label htmlFor="Category">
+        Category
+        <select name="Category" id="Category">
+          <option value="Action">Action</option>
+          <option value="Science Fiction">Science Fiction</option>
+          <option value="Economy">Economy</option>
+        </select>
+      </label>
 
-        <button
-          type="submit"
-          onClick={() => {
-            // submitBookToStore();
-          }}
-        >
-          Add Book
-        </button>
-      </form>
+      <button
+        type="submit"
+        onClick={(e) => {
+          e.preventDefault();
+          submitBookToStore();
+        }}
+      >
+        Add Book
+      </button>
+    </form>
   );
 }
 
