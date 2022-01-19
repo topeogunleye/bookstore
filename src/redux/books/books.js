88ff5cb9e2a6633305984fe3dd85c2477540e55d
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-01-19 11:07:15 +0100

    Add ADD_BOOK and REMOVE_BOOK logic

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,7 +3,31 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
-const initialState = [];
+const initialState = {
+  books: [
+    {
+      title: 'The Hunger Games',
+      author: 'Suzanne Collins',
+      category: 'Action',
+      completed: false,
+      id: 1,
+    },
+    {
+      title: 'Dune',
+      author: 'Frank Herbert',
+      category: 'Science Fiction',
+      completed: false,
+      id: 2,
+    },
+    {
+      title: 'Capital in the Twenty First Century',
+      author: 'Thomas Piketty',
+      category: 'Economy',
+      completed: false,
+      id: 3,
+    },
+  ],
+};
 
 export const addBook = (payload) => ({
   type: ADD_BOOK,
@@ -18,9 +42,11 @@ export const removeBook = (payload) => ({
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
-      return [...state, action.payload];
+      return { books: [...state.books, action.payload] };
     case REMOVE_BOOK:
-      return [...state, action.payload];
+      return {
+        books: state.books.filter(({ id }) => id !== action.payload),
+      };
     default:
       return state;
   }
