9afbc7023b523eb180f92f0cd4038078d7915065
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-01-20 22:12:01 +0100

    Call Dispatch fetchBooks in Books component

--- src/components/books/Books.js
+++ src/components/books/Books.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchBooks } from '../../redux/books/books';
 import Book from '../book/Book';
 import AddNewBook from '../form/AddNewBook';
 
 function Books() {
+  const dispatch = useDispatch();
+
   // GET books from redux store
-  const books = useSelector((state) => state.booksReducer.books);
+  const books = useSelector((state) => state.booksReducer);
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, []);
 
   return (
     <div>
@@ -14,11 +21,9 @@ function Books() {
         {books.map((book) => (
           <Book
             category={book.category}
-            completed={book.completed}
-            author={book.author}
-            id={book.id}
+            id={book.item_id}
             title={book.title}
-            key={book.id}
+            key={book.item_id}
           />
         ))}
       </ul>
