1ae3a9fb292af8ac22be742b59b077854ed6af12
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-01-20 04:28:01 +0100

    Set up Async Actions

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,8 +1,42 @@
 // books.js
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const FETCH_USERS_REQUEST = 'FETCH_USERS_REQUEST';
+const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
+const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE';
+
+export const fetchUsersRequest = () => {
+  return {
+    type: FETCH_USERS_REQUEST
+  }
+}
+
+export const fetchUsersSuccess = () => {
+  return {
+    type: FETCH_USERS_SUCCESS,
+    payload: users
+  }
+}
+
+export const fetchUsersFailure = error => {
+  return {
+    type: FETCH_USERS_FAILURE,
+    payload: error
+  }
+}
+
+export const addBook = (payload) => ({
+  type: ADD_BOOK,
+  payload,
+});
+
+export const removeBook = (payload) => ({
+  type: REMOVE_BOOK,
+  payload,
+});
 
 const initialState = {
+  loading: true,
   books: [
     {
       title: 'The Hunger Games',
@@ -26,27 +60,37 @@ const initialState = {
       id: 3,
     },
   ],
+  error: ''
 };
 
-export const addBook = (payload) => ({
-  type: ADD_BOOK,
-  payload,
-});
 
-export const removeBook = (payload) => ({
-  type: REMOVE_BOOK,
-  payload,
-});
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_USERS_REQUEST: 
+      return {
+        ...state,
+        loading: true
+      }
+    case FETCH_USERS_SUCCESS:
+      return {
+        loading: false,
+        books: action.payload,
+        error: ''
+      }
+    case FETCH_USERS_FAILURE:
+      return {
+        loading: false,
+        books: [],
+        error: action.payload
+      }
     case ADD_BOOK:
       return { books: [...state.books, action.payload] };
     case REMOVE_BOOK:
       return {
         books: state.books.filter(({ id }) => id !== action.payload),
       };
-    default:
+    default: 
       return state;
   }
 };
