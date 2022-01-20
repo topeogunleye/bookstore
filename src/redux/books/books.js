0edd4bda76fb357b059b271675819c95207b9708
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-01-20 22:14:50 +0100

    Add fetchBook, postBook, and deleteBook actions

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,29 +1,10 @@
+import axios from 'axios';
+
 // books.js
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
-const FETCH_USERS_REQUEST = 'FETCH_USERS_REQUEST';
-const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
-const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE';
 
-export const fetchUsersRequest = () => {
-  return {
-    type: FETCH_USERS_REQUEST
-  }
-}
-
-export const fetchUsersSuccess = () => {
-  return {
-    type: FETCH_USERS_SUCCESS,
-    payload: users
-  }
-}
-
-export const fetchUsersFailure = error => {
-  return {
-    type: FETCH_USERS_FAILURE,
-    payload: error
-  }
-}
+const initialState = [];
 
 export const addBook = (payload) => ({
   type: ADD_BOOK,
@@ -32,65 +13,51 @@ export const addBook = (payload) => ({
 
 export const removeBook = (payload) => ({
   type: REMOVE_BOOK,
-  payload,
+  item_id: payload.id,
 });
 
-const initialState = {
-  loading: true,
-  books: [
-    {
-      title: 'The Hunger Games',
-      author: 'Suzanne Collins',
-      category: 'Action',
-      completed: false,
-      id: 1,
-    },
-    {
-      title: 'Dune',
-      author: 'Frank Herbert',
-      category: 'Science Fiction',
-      completed: false,
-      id: 2,
-    },
-    {
-      title: 'Capital in the Twenty First Century',
-      author: 'Thomas Piketty',
-      category: 'Economy',
-      completed: false,
-      id: 3,
-    },
-  ],
-  error: ''
+export const fetchBooks = () => (dispatch) => {
+  axios
+    .get(
+      'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/VPg2NHR7FOEaX70FWtik/books/',
+    )
+    .then((response) => {
+      const books = response.data;
+      Object.keys(books).forEach((itemId) => {
+        const [book] = books[itemId];
+        book.item_id = itemId;
+        dispatch(addBook(book));
+      });
+    })
+    .catch(() => {});
 };
 
+export const postBook = (newBook) => (dispatch) => {
+  axios
+    .post(
+      'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/VPg2NHR7FOEaX70FWtik/books/',
+      newBook,
+    )
+    .then(() => dispatch(addBook(newBook)))
+    .catch(() => {});
+};
 
+export const deleteBook = (id) => (dispatch) => {
+  axios
+    .delete(
+      `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/VPg2NHR7FOEaX70FWtik/books/${id}`,
+    )
+    .then(() => dispatch(removeBook({ id })))
+    .catch(() => {});
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case FETCH_USERS_REQUEST: 
-      return {
-        ...state,
-        loading: true
-      }
-    case FETCH_USERS_SUCCESS:
-      return {
-        loading: false,
-        books: action.payload,
-        error: ''
-      }
-    case FETCH_USERS_FAILURE:
-      return {
-        loading: false,
-        books: [],
-        error: action.payload
-      }
     case ADD_BOOK:
-      return { books: [...state.books, action.payload] };
+      return [...state, action.payload];
     case REMOVE_BOOK:
-      return {
-        books: state.books.filter(({ id }) => id !== action.payload),
-      };
-    default: 
+      return state.filter((book) => book.item_id !== action.item_id);
+    default:
       return state;
   }
 };
