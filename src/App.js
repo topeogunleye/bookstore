8afdc1a79705b0dbcdf2b0bb59b9f9173d8a099a
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-01-22 02:12:39 +0100

    Add classname to App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Categories from './components/categories/Categories';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App Bookstore-CMS">
       <BrowserRouter>
         <Header />
         <Routes>
